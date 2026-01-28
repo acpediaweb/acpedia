b635f3aa491fbe973b52e7f810d3a7dbcdb031b5
acpediaweb <acpediaweb@gmail.com>
2026-01-28 12:22:56 +0700

    ggg

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\CLIRequest;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class BaseController
+ */
+abstract class BaseController extends Controller
+{
+    protected $request;
+    protected $helpers = [];
+
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+
+        // Preload any helpers, libraries, etc. here:
+        // $this->session = \Config\Services::session();
+    }
+}
