586c80394d6e5617d08141363dd43841c549f3a8
acpediaweb <acpediaweb@gmail.com>
2026-01-28 09:04:19 +0700

    eei

--- app/Models/BrandModel.php
+++ app/Models/BrandModel.php
@@ -4,22 +4,10 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class InventoryLogModel extends Model
+class BrandModel extends Model
 {
-    protected $table            = 'inventory_logs';
+    protected $table            = 'brands';
     protected $primaryKey       = 'id';
-    protected $allowedFields    = ['inventory_id', 'action_title', 'action_description', 'action_actor'];
-    protected $useTimestamps    = false; // We use action_timestamp from SQL default
-
-    /**
-     * Fetch the "Discussion/Timeline" for a specific unit
-     */
-    public function getUnitHistory($inventoryId)
-    {
-        return $this->select('inventory_logs.*, users.fullname as actor_name, users.profile_picture')
-                    ->join('users', 'users.id = inventory_logs.action_actor', 'left')
-                    ->where('inventory_id', $inventoryId)
-                    ->orderBy('action_timestamp', 'DESC')
-                    ->findAll();
-    }
+    protected $allowedFields    = ['brand_name', 'brand_description', 'brand_logo'];
+    protected $useTimestamps    = true;
 }
